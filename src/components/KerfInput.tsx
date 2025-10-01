ce62aded0fcc88ae0685057c9999e4897a86eccd
Nebras Khan <khann84@mcmaster.ca>
2025-10-01 15:03:00 -0400

    feat: add kerf width input component for blade offset

---
+++ src/components/KerfInput.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import InputNumber from "./InputNumber";
+
+type Props = {
+  value: number;
+  onChange: (v: number) => void;
+};
+
+export default function KerfInput({ value, onChange }: Props) {
+  return (
+    <InputNumber
+      label="Kerf (Cut Width)"
+      value={value}
+      onChange={onChange}
+      min={0}
+      step={0.1}
+      suffix="mm"
+      placeholder="e.g., 3.2"
+      required
+    />
+  );
+}
